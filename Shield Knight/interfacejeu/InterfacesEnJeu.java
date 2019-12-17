ff472e8e96431caa9435623eb04abad39ca315a0
Kristo <christophe.allaire@hotmail.com>
2019-12-16 20:27:19 -0500

    Maps working 1.4

--- Shield Knight/interfacejeu/InterfacesEnJeu.java
+++ Shield Knight/interfacejeu/InterfacesEnJeu.java
@@ -4,6 +4,7 @@ import src.*;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class InterfacesEnJeu {
